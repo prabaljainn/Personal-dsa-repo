abf07089f50d86f781ba33963116da8f0d2ede05
Prabal Jain <prabaljainn@gmail.com>
2021-08-19 08:58:57 +0530

    added some things

--- practice/p.cpp
+++ practice/p.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,11 +8,23 @@ int main()
     cin >> testcase;
     while (testcase--)
     {
-        int xa, xb, Xa, Xb;
-        cin >> xa >> xb >> Xa >> Xb;
-        int num1 = Xa / xa;
-        int num2 = Xb / xb;
-        cout << num1 + num2 << "\n";
+        int arr[4];
+        for (int i = 0; i < 4; i++)
+        {
+            cin >> arr[i];
+        }
+        unordered_set<int> s(arr, arr + 4);
+        int number_of_d_elem = s.size();
+        if (number_of_d_elem == 1 && arr[0] == arr[1] && arr[1] == arr[2])
+        {
+            number_of_d_elem = 0;
+        }
+        if (number_of_d_elem == 0)
+            cout << 0 << endl;
+        if (number_of_d_elem == 1)
+            cout << 1 << endl;
+        if (number_of_d_elem == 4 || number_of_d_elem == 2)
+            cout << 2 << endl;
     }
     return 0;
 }
